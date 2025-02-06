a19504cca0d881212e16e9209bd880a094aa7d47
Henri Vainio <matikkaeditorinkaantaja@gmail.com>
2025-02-07 00:08:12 +0200

    Move auth service project files back to main project

---
+++ LarpakeServer/Identity/EntraId/EntraAuthenticationServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace LarpakeServer.Identity.EntraId;
+
+public static class EntraAuthenticationServiceExtensions
+{
+    public static IServiceCollection AddEntraAuthenticationService(
+        this IServiceCollection services, IConfiguration config)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(
+                config.GetSection(EntraIdOptions.SectionName));
+
+        return services;
+    }
+}
+    
