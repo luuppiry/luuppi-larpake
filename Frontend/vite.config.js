ba7874c87bc2c8f41b55448dae21711720aab29b
Henri Vainio <matikkaeditorinkaantaja@gmail.com>
2025-02-23 23:45:06 +0200

    Make proxy more permissive

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
     server: {
         port: 3000,
         proxy: {
-            "^/[^/]+\\.html$": {
+            "^/(?!.*(fi/|en/)).+\\.html$": {
                 target: "http://localhost:3000",
                 changeOrigin: true,
                 rewrite: (path) => {
