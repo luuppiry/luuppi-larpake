f51ecf85026d82495c88c265ce557a53c3968007
Henri Vainio <matikkaeditorinkaantaja@gmail.com>
2025-02-28 19:29:53 +0200

    Comment form container

--- Frontend/src/components/form-container.ts
+++ Frontend/src/components/form-container.ts
@@ -3,20 +3,27 @@ class FormContainer extends HTMLElement {
 
     constructor() {
         super();
+        // Add mutation observer to track changes to the element
         this.observer = new MutationObserver((_) => this.appendContent());
     }
 
     connectedCallback() {
+        // Add observer to track if element children are changed
         this.observer.observe(this, { childList: true });
     }
 
     disconnectedCallback() {
+        // disconnect when element is not rendered
         this.observer.disconnect();
     }
 
     appendContent() {
+        /* This method should be only called by mutation observer
+         * Method reads inner html of the component and moves it to correct palce
+         */
         const content = this.innerHTML;
 
+        // Disconnect observer because no later changes should be tracked (causes stack overflow)
         this.observer.disconnect();
         this.innerHTML = `
         <div class="hor-center-content">
