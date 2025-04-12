0218a3209bf6f252647ef78f5caf79beeb256f7a
Henri Vainio <matikkaeditorinkaantaja@gmail.com>
2025-04-12 22:54:18 +0300

    Remove 404 page for now

--- LarpakeServer/Middleware/StaticFileServer.cs
+++ LarpakeServer/Middleware/StaticFileServer.cs
@@ -10,33 +10,25 @@ public static class StaticFileServer
             {
                 if (ctx.File?.Name is null)
                 {
-                    logger.LogInformation("Static file name is null");
                     return;
                 }
                 if (ctx.File.Name.EndsWith(".html"))
                 {
-                    logger.LogInformation("Static file is html file, {file}", ctx.File.Name);
                     ctx.Context.Response.ContentType = "text/html; charset=utf-8";
                     return;
                 }
                 if (ctx.File.Name.EndsWith(".js"))
                 {
-                    logger.LogInformation("Static file is js file, {file}", ctx.File.Name);
                     ctx.Context.Response.ContentType = "application/javascript";
                     return;
                 }
                 if (ctx.File.Name.EndsWith(".css"))
                 {
-                    logger.LogInformation("Static file is css file, {file}", ctx.File.Name);
                     ctx.Context.Response.ContentType = "text/css";
                     return;
                 }
-                logger.LogInformation("File is not of any type, {file}", ctx.File.Name);
-
             }
         });
-
-        app.UseStatusCodePagesWithReExecute("/fi/404.html");
 
         //app.UseStatusCodePages((builder) =>
         //{
