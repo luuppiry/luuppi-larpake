24ca8c0c7343d6607680085b661ac2c01fd67b5f
Henri Vainio <matikkaeditorinkaantaja@gmail.com>
2025-04-17 23:12:52 +0300

    Add IsFreshmanEvent to org event db, fix paging

---
+++ LarpakeServer/Models/IdQueryObject/EventIdObject.cs
@@ -0,0 +1,8 @@
+﻿using LarpakeServer.Models.Localizations;
+
+namespace LarpakeServer.Models.IdQueryObject;
+
+public class EventIdObject : OrganizationEventLocalization
+{
+    public long OrganizationEventId { get; set; }
+}
