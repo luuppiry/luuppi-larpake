7b5b53a8f2a6e18ea69ab5a0a05f42991e5fdc00
Henri Vainio <matikkaeditorinkaantaja@gmail.com>
2025-04-27 23:27:21 +0300

    Comment home.ts

--- Frontend/src/home.ts
+++ Frontend/src/home.ts
@@ -4,17 +4,23 @@ import { navigateTo } from "./helpers.js";
 import SidePanel from "./components/side-panel.js";
 import { INDEX_LINK_ID } from "./components/side-panel-data.js";
 
+// Set header index page redirect to not redirect back to home
 const header = document.getElementById(UI_HEADER_ID) as Header;
 header.setNavigateHome(() => {
     navigateTo(`index.html?${Q_NO_TOKEN}=false`);
 });
 
+// Set side panel page redirect to not redirect back to home
 const sidePanel = document.getElementById(SIDE_PANEL_ID) as SidePanel;
 sidePanel.setLoaded((p) => {
     const items = p.queryMenuItems(`.${INDEX_LINK_ID}`);
+
     items.forEach((x) => {
-        x.addEventListener("click", (_) => {
-            navigateTo(`index.html?${Q_NO_TOKEN}=false`);
-        });
+        for (const child of x.children) {
+            const link = child as HTMLAnchorElement;
+            if (link) {
+                link.href = `index.html?${Q_NO_TOKEN}=false`;
+            }
+        }
     });
 });
