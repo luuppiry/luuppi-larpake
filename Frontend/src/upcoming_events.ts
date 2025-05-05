3703ddba34657e50cab44312148369a1318d2805
Juliusz Kotelba <juliusz.kotelba@tuni.fi>
2025-05-05 23:12:42 +0300

    Updated upcoming events page

---
+++ Frontend/src/upcoming_events.ts
@@ -0,0 +1,24 @@
+import EventPreviewer from "./components/event-previewer.js";
+import { EventClient } from "./api_client/event_client.js";
+
+const eventClient = new EventClient();
+
+async function loadComingEvents() {
+    const container = document.getElementById("event-container") as HTMLUListElement;
+    if (!container) {
+        throw new Error("Event container not found, check naming.");
+    }
+
+    const events = await eventClient.nextComing();
+    if (!events) {
+        throw new Error("Failed to load coming events");
+    }
+
+    for (const event of events.sort((x, y) => (x.startsAt > y.startsAt ? 1 : -1))) {
+        const elem = document.createElement("li", { is: "event-previewer" }) as EventPreviewer;
+        container.appendChild(elem);
+        elem.setData(event);
+    }
+}
+
+await loadComingEvents();
